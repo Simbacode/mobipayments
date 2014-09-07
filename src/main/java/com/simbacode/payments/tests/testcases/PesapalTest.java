6a647d113dfd0619d2af605cb3daf4db3c6d3fbf
Acellam <abiccel@yahoo.com>
2014-09-07 12:45:56 +0300

    connetion to pesapal is ok but error of invalid signature method.started to work on junit

---
+++ src/main/java/com/simbacode/payments/tests/testcases/PesapalTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simbacode.payments.tests.testcases;
+
+import com.simbacode.payments.oauth.Pesapal;
+
+/**
+ *
+ * @author Acellam
+ */
+public class PesapalTest {
+
+}
---
+++ src/main/java/com/simbacode/payments/tests/testsuits/mobipayments.java
@@ -0,0 +1,11 @@
+package com.simbacode.payments.tests.testsuits;
+
+import com.simbacode.payments.tests.testcases.PesapalTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({PesapalTest.class})
+public class mobipayments {
+}
