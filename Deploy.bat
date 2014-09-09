601276c754c08f58475eb6676c8126eb7f59bb09
Acellam Guy <abiccel@yahoo.com>
2014-09-09 17:18:10 +0300

    maven

--- Deploy.bat
+++ Deploy.bat
@@ -1,1 +1,7 @@
+ssh-agent
+
+ssh-add C:\Users\User\.ssh\id_rsa.pub
+
 mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+
+ssh-add -D
--- Deploy.sh
+++ Deploy.sh
@@ -5,6 +5,8 @@
 read -p "Really deploy to maven cetral repository  (yes/no)? "
 
 if ( [ "$REPLY" == "yes" ] ) then
+  ssh-add ~/.ssh/id_rsa.pub
+  eval 'ssh-agent -s'
   ssh-add -l
   mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
