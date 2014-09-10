1c632cdfac56e9c7458ad38c8ddc9073f6eadc8e
Acellam Guy <abiccel@yahoo.com>
2014-09-10 18:15:34 +0300

    first dummy test for the pesapal iframe

---
+++ src/test/java/com/simbacode/pesapal/testcases/PesapalTest.java
@@ -0,0 +1,22 @@
+/*
+ * This is the main class for testing for the pesapal iframe.
+ *
+ */
+package com.simbacode.pesapal.testcases;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Acellam
+ */
+public class PesapalTest extends TestCase {
+
+    /**
+     * Rigourous Test
+     */
+    public void testIframe()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/com/simbacode/pesapal/testsuits/MobipaymentsTest.java
@@ -0,0 +1,12 @@
+package com.simbacode.pesapal.testsuits;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.simbacode.pesapal.testcases.PesapalTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PesapalTest.class })
+public class MobipaymentsTest {
+}
